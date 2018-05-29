7152a4b45f44cf4a200231484bae293f8b4ee4bc
Vincent Jacques <vincent@vincent-jacques.net>
2018-05-29 09:18:04 +0200

    Generate index.html

--- generate.py
+++ generate.py
@@ -1,3 +1,24 @@
 #!/usr/bin/env python3
 
+import sys
+import os
+
 import jinja2
+
+environment = jinja2.Environment(loader=jinja2.FileSystemLoader("templates"))
+
+def render(*, template, destination, **context):
+    with open(os.path.join(sys.argv[1], destination), "w") as f:
+        f.write(environment.get_template(template).render(context))
+        f.write("\n")
+
+render(
+    template="index.html",
+    destination="index.html",
+    sections=[
+        dict(slug="musique", title="Musique"),
+        dict(slug="cinema", title="Cinéma"),
+        dict(slug="theatre", title="Théâtre"),
+        dict(slug="expositions", title="Expositions"),
+    ],
+)
