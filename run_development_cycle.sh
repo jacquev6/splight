e02a00a393ea61580c912001999e3f0338c02dc6
Vincent Jacques <vincent@vincent-jacques.net>
2018-05-30 08:00:09 +0200

    Script: relaunch web server on rebuild

--- run_development_cycle.sh
+++ run_development_cycle.sh
@@ -4,15 +4,42 @@
 
 set -o errexit
 
+PROJECT_ROOT=$(pwd)
+
+SHOW_IN_BROWSER=false
+function show_in_browser {
+  echo
+  echo "$1: $2"
+  echo
+  if $SHOW_IN_BROWSER
+  then
+    python -m webbrowser -t $2
+  fi
+}
+
+while [[ "$#" > 0 ]]
+do
+  case $1 in
+    -wb|--web-browser)
+      SHOW_IN_BROWSER=true
+      ;;
+    *)
+      echo "Unknown parameter passed: $1"
+      exit 1;;
+  esac
+  shift
+done
+
 python3 -m unittest --verbose generator.tests
+
+fuser -k 4000/tcp >/dev/null 2>/dev/null || true
 
 python3 -m generator . docs
 
 git diff --ignore-all-space --ignore-space-at-eol --ignore-blank-lines --stat docs
 
-cd docs
-# python -m SimpleHTTPServer 4000
-cd ..
+(cd docs; python -m SimpleHTTPServer 4000) >>web.log 2>&1 &
+show_in_browser "Website" "http://localhost:4000"
 
 pycodestyle --max-line-length=120 $(git ls-files "*.py")
 
