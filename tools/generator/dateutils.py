3ac12792570b040e6ea400f99f07d1565e33e903
Vincent Jacques <vincent@vincent-jacques.net>
2018-05-31 12:57:35 +0200

    Clean-up unused code

--- tools/generator/dateutils.py
+++ tools/generator/dateutils.py
@@ -2,13 +2,6 @@ import datetime
 import unittest
 
 
-# https://stackoverflow.com/a/38283685/905845
-def iso_to_gregorian(iso_year, iso_week, iso_day):
-    jan4 = datetime.date(iso_year, 1, 4)
-    start = jan4 - datetime.timedelta(days=jan4.isoweekday()-1)
-    return start + datetime.timedelta(weeks=iso_week-1, days=iso_day-1)
-
-
 def previous_week_day(date, weekday):
     while date.weekday() != weekday:
         date -= datetime.timedelta(days=1)
