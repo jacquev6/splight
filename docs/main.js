6e544f2f0bc93504ff3ceb19898b7ee54d119c31
Vincent Jacques <vincent@vincent-jacques.net>
2018-06-19 16:17:47 +0200

    Filter events by tag

--- docs/main.js
+++ docs/main.js
@@ -1,6 +1,6 @@
 var Splight = (function() {
-  function make_event_source() {
-    var cache = {
+  function make_events_cache() {
+    return {
       days: {},
 
       get: function(start, end, callback) {
@@ -20,7 +20,6 @@ var Splight = (function() {
         }
 
         if(keys_to_fetch.size > 0) {
-          console.log("Cache misses:", keys_to_fetch);
           keys_to_fetch.forEach(function(k) {
             $.getJSON(
               "/reims/" + k + ".json",
@@ -37,7 +36,6 @@ var Splight = (function() {
             )
           });
         } else {
-          console.log("Full cache hit");
           self.call(required_keys, callback);
         }
       },
@@ -53,10 +51,6 @@ var Splight = (function() {
 
         callback(events);
       }
-    };
-
-    return function(start, end, timezone, callback ) {
-      cache.get(start, end, callback);
     };
   }
 
@@ -76,13 +70,32 @@ var Splight = (function() {
         self.displayed_week = config.displayed_week.start_date;
         self.first_week = config.first_week.start_date;
         self.week_after = config.week_after.start_date;
+        self.events_cache = make_events_cache();
+        var query = URI.parseQuery(URI.parse(window.location.href).query);
+        self.display_all_tags = false;
+        self.displayed_tags = new Set(Object.keys(query));
+        if(self.displayed_tags.size == 0) {
+          self.display_all_tags = true;
+          self.displayed_tags = new Set($("input[name=displayed_tags]").map((x, y) => $(y).val()).toArray());
+        }
       } else {
         self.displayed_week = null;
       }
 
-      // @todo Password-protect admin mode
-      $("#sp-admin-enter").on("click", function() {self.is_admin = true; self.update_browser()});
-      $("#sp-admin-quit").on("click", function() {self.is_admin = false; self.update_browser()});
+      $("#sp-admin-enter").on("click", function() {
+        // @todo Password-protect admin mode
+        self.is_admin = true;
+        self.update_browser();
+      });
+      $("#sp-admin-quit").on("click", function() {
+        self.is_admin = false;
+        self.update_browser();
+      });
+      $("input[name=displayed_tags]").on("change", function() {
+        self.displayed_tags = new Set($("input[name=displayed_tags]:checked").map((x, y) => $(y).val()).toArray());
+        self.display_all_tags = $("input[name=displayed_tags]:not(:checked)").length == 0;
+        self.update_browser();
+      });
 
       if(self.displayed_week) {
         $("#sp-fullcalendar").fullCalendar({
@@ -92,7 +105,11 @@ var Splight = (function() {
           locale: "fr",
           allDaySlot: false,
           height: "auto",
-          events: make_event_source(),
+          events: function(start, end, timezone, callback) {
+            self.events_cache.get(start, end, function(events) {
+              callback(events.filter(e => e.tags.some(t => self.displayed_tags.has(t))));
+            });
+          },
           views: {
             agendaThreeDays: {
               type: "agenda",
@@ -118,16 +135,26 @@ var Splight = (function() {
     update_browser: function(initial) {
       var self = this;
 
-      self.fix_admin();
+      self.update_admin();
+      self.update_display_settings();
       if(!initial) self.update_calendar();
-      self.fix_links();
+      self.update_links();
     },
 
-    fix_admin: function() {
+    update_admin: function() {
       var self = this;
 
       Cookies.set("admin", self.is_admin);
       $("#sp-admin").toggle(self.is_admin);
+    },
+
+    update_display_settings: function() {
+      var self = this;
+
+      $("input[name=displayed_tags]").each(function(index, input) {
+        var input = $(input);
+        input.prop("checked", self.display_all_tags || self.displayed_tags.has(input.val()));
+      });
     },
 
     update_calendar: function() {
@@ -136,15 +163,24 @@ var Splight = (function() {
       self.calendar.refetchEvents();
     },
 
-    fix_links: function() {
+    update_links: function() {
       var self = this;
 
       if(self.city) {
-        function fix_link_class(kwds) {
+        var new_query = self.displayed_week ? (self.display_all_tags ? "" : Array.from(self.displayed_tags).join("&")) : undefined;
+
+        function make_new_path(week) {
+          return "/" + self.city + week.format("/GGGG-[W]WW/");
+        }
+
+        function update_link_class(kwds) {
           var links = $(kwds.selector);
-          var new_path = "/" + self.city + kwds.week.format("/GGGG-[W]WW/");
+          var new_path = make_new_path(kwds.week);
           links.prop("href", function(index, href) {
-            return URI(href).path(new_path).toString();
+            var uri = URI(href);
+            uri.path(new_path)
+            if(self.displayed_week) uri.query(new_query);
+            return uri.toString();
           });
           if(kwds.global_condition && kwds.non_admin_condition) {
             links.show();
@@ -157,7 +193,7 @@ var Splight = (function() {
           }
         }
 
-        fix_link_class({
+        update_link_class({
           selector: ".sp-now-week-link",
           week: moment(),
           global_condition: true,
@@ -165,8 +201,10 @@ var Splight = (function() {
         });
 
         if(self.displayed_week) {
+          history.replaceState(null, window.document.title, URI(window.location).path(make_new_path(self.displayed_week)).query(new_query).toString());
+
           var previous_week = self.displayed_week.clone().subtract(1, "week");
-          fix_link_class({
+          update_link_class({
             selector: ".sp-previous-week-link",
             week: previous_week,
             global_condition: previous_week >= self.first_week,
@@ -174,7 +212,7 @@ var Splight = (function() {
           });
 
           var next_week = self.displayed_week.clone().add(1, "week");
-          fix_link_class({
+          update_link_class({
             selector: ".sp-next-week-link",
             week: next_week,
             global_condition: next_week < self.week_after,
