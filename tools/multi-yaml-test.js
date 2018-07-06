3347c80f5ad91216448d068bf6385cc9e6db7e19
Vincent Jacques <vincent@vincent-jacques.net>
2018-07-06 10:25:12 +0200

    Translate multi-yaml to JavaScript

--- tools/multi-yaml-test.js
+++ tools/multi-yaml-test.js
@@ -1,11 +1,35 @@
-var assert = require("assert");
+"use strict";
+const assert = require("assert").strict;
+const fs = require("fs");
+const path = require('path');
 
-var m = require("./multi-yaml");
+const yaml = require("js-yaml");
+
+const multi_yaml = require("./multi-yaml");
+
 
 describe("multi-yaml", function() {
-  describe("foo", function() {
-    it("should return 42", function() {
-      assert.equal(m.foo(), 42);
-    });
-  });
+  describe("load", function() {
+    fs.readdirSync("multi-yaml-test/outputs").forEach(function(name) {
+      const [base_name, status, ext] = name.split(".");
+      const input_name = path.join("multi-yaml-test", "inputs", base_name);
+      const output_name = path.join("multi-yaml-test", "outputs", name);
+      const expected = yaml.safeLoad(fs.readFileSync(output_name));
+      if(status == "ok") {
+        it("should load '" + base_name + "'", function() {
+          assert.deepEqual(multi_yaml.load(input_name), expected);
+        });
+      } else {
+        it("should fail to load '" + base_name + "'", function() {
+          try {
+            multi_yaml.load(input_name);
+          } catch(e) {
+            assert.equal(e.message, expected);
+            return;
+          }
+          assert.fail("Exception not raised");
+        });
+      }
+    })
+  })
 });
--- tools/multi-yaml.js
+++ tools/multi-yaml.js
@@ -1,7 +1,100 @@
-exports.foo = function() {
-    return 42;
+"use strict";
+const fs = require("fs");
+const path = require('path');
+
+const yaml = require("js-yaml");
+
+
+function merge_data(data, new_data) {
+  if(!data) {
+    return new_data;
+  } else if(Array.isArray(data) && Array.isArray(new_data)) {
+    return data.concat(new_data);
+  } else if(Array.isArray(data) && typeof(new_data) == "object") {
+    for(const k in new_data) {
+      data.push(new_data[k]);
+    }
+    return data
+  } else if(typeof(data) == "object" && typeof(new_data) == "object") {
+    for(const k in new_data) {
+      const v = data[k];
+      if(v) {
+        data[k] = merge_data(v, new_data[k]);
+      } else {
+        data[k] = new_data[k];
+      }
+    }
+    return data;
+  } else {
+    throw new Error("Types incompatible for merging: " + typeof(data) + " and " + typeof(new_data));
+  }
 }
 
-exports.bar = function() {
-    return 57;
+function load(dir_name) {
+  const extensions = [".json", ".yml", ".yaml"];
+  const no_data_to_load = "No data to load";
+
+  const [is_file, file_contents] = (function() {
+    var is_file = false;
+    const file_contents = [];
+
+    extensions.forEach(function(ext) {
+      [dir_name + ext, dir_name + "/" + ext].forEach(function(file_name) {
+        try {
+          file_contents.push(fs.readFileSync(file_name));
+          is_file = true;
+        } catch {
+        }
+      });
+    });
+
+    return [is_file, file_contents];
+  })();
+
+  const [is_directory, directory_contents] = (function() {
+    try {
+      return [true, fs.readdirSync(dir_name)];
+    } catch {
+      return [false, []];
+    }
+  })();
+
+  var data = null;
+
+  file_contents.forEach(function(contents) {
+    data = merge_data(data, yaml.safeLoad(contents));
+  });
+
+  if(is_directory) {
+    const new_data = {};
+    directory_contents.map(name => {
+      const key = (function() {
+        const [radix, ext] = name.split(".");
+        if(extensions.indexOf("." + ext) != -1) {
+          return radix;
+        } else {
+          return name;
+        }
+      })();
+
+      if(key) {
+        try {
+          new_data[key] = load(dir_name + "/" + key);
+        } catch(e) {
+          if(e.message != no_data_to_load) {
+            throw e;
+          }
+        }
+      }
+    });
+    data = merge_data(data, new_data);
+  };
+
+  if(is_file || is_directory) {
+    return data;
+  } else {
+    throw new Error(no_data_to_load);
+  }
 }
+
+exports.load = load;
