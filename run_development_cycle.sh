2cc4686059b456e076b6d7a8da2d673a03ee4905
Vincent Jacques <vincent@vincent-jacques.net>
2018-07-07 10:56:46 +0200

    Focus on a test data set

--- run_development_cycle.sh
+++ run_development_cycle.sh
@@ -32,6 +32,7 @@ done
 
 show_in_browser "Unit test coverage details" $PROJECT_ROOT/tools/coverage/index.html
 
-(cd tools; npm run generator ../data ../docs)
+(cd tools; npm run generator test-data test-site)
 
-git --no-pager diff --ignore-all-space --ignore-space-at-eol --ignore-blank-lines --stat docs
+# (cd tools; npm run generator ../data ../docs)
+# git --no-pager diff --ignore-all-space --ignore-space-at-eol --ignore-blank-lines --stat docs
