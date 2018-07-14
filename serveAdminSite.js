10ea38b45a23009da94847c248f88686b6f902c6
Vincent Jacques <vincent@vincent-jacques.net>
2018-07-14 10:25:13 +0200

    Quit server when last browser disconnects

--- serveAdminSite.js
+++ serveAdminSite.js
@@ -1,10 +1,12 @@
 'use strict'
 
 const fs = require('fs-extra')
+const http = require('http')
 const path = require('path')
 
 const express = require('express')
 const opn = require('opn')
+const ws = require('ws')
 
 require('stringify').registerWithRequire(['.html'])
 
@@ -20,6 +22,7 @@ async function serve () {
   const htmlGenerator = generator.html.generator(multiYaml.load(process.argv[2]), true)
 
   const app = express()
+  const server = http.Server(app)
 
   require('reload')(app)
 
@@ -56,7 +59,36 @@ async function serve () {
     async (req, res) => res.send(await htmlGenerator.timespanPage(req.params.city, req.params.timespan))
   )
 
-  app.listen(8000, () => {
+  const wss = new ws.Server({
+    port: 8080,
+    perMessageDeflate: false
+  })
+
+  var connectedUsers = 0
+  wss.on('connection', socket => {
+    connectedUsers++
+    console.log('Connection => ', connectedUsers, 'connected users')
+    socket.on('close', () => {
+      connectedUsers--
+      console.log('Disconnection => ', connectedUsers, 'connected users')
+      if (connectedUsers === 0) {
+        console.log('Maybe closing server in a second')
+        setTimeout(() => {
+          if (connectedUsers === 0) {
+            console.log('Closing server')
+            server.close(() => {
+              console.log('Server closed')
+              process.exit(0)
+            })
+          } else {
+            console.log('Saving server')
+          }
+        }, 1000)
+      }
+    })
+  })
+
+  server.listen(8000, () => {
     const address = 'http://localhost:8000/'
     console.log('Admin site live at', address)
     opn(address)
