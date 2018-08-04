0cebc6cadf503bc9a1d9b0f86e22404be431a6af
Vincent Jacques <vincent@vincent-jacques.net>
2018-08-04 09:12:34 +0200

    Reduce font size of tags in event details

--- splight/publicWebsite/widgets/eventDetails/what.js
+++ splight/publicWebsite/widgets/eventDetails/what.js
@@ -4,8 +4,6 @@ const mustache = require('mustache')
 
 const template = require('./what.html')
 
-// @todo Use custom-colored badges (https://getbootstrap.com/docs/4.1/components/badge/) for the tags
-
 function render ({city, event}) {
   return mustache.render(
     template,
