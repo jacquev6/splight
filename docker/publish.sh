e73c747edb9005b4b65e0731f307f1275e3eaa5d
Vincent Jacques <vincent@vincent-jacques.net>
2018-12-08 10:37:43 +0100

    Run it on GCP!

--- docker/publish.sh
+++ docker/publish.sh
@@ -19,4 +19,12 @@ echo "--------------------------------------------"
 echo "Produced splight-admin:$TAG"
 echo "--------------------------------------------"
 
-docker run --rm --name splight-admin --publish 8000:80 splight-admin:$TAG
+docker tag splight-admin:$TAG eu.gcr.io/splight-0001/splight-admin:$TAG
+
+docker push eu.gcr.io/splight-0001/splight-admin:$TAG
+
+echo "--------------------------------------------"
+echo "Pushed splight-admin:$TAG"
+echo "--------------------------------------------"
+
+# docker run --rm --name splight-admin --publish 8000:80 splight-admin:$TAG
