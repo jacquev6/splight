3caaf4fca862643f82683f0d79186ca8b8740718
Vincent Jacques <vincent@vincent-jacques.net>
2019-02-04 20:11:54 +0100

    Publish to hub.docker.io

--- publish.sh
+++ publish.sh
@@ -4,39 +4,39 @@ set -o errexit
 
 TAG=$(date "+%Y%m%d-%H%M%S")
 
-sed -i "s/[0-9]\{8\}-[0-9]\{6\}/$TAG/g" splight/adminWebsite/page.html
-sed -i "s/:[0-9]\{8\}-[0-9]\{6\}/:$TAG/g" *prod.yml
+# sed -i "s/[0-9]\{8\}-[0-9]\{6\}/$TAG/g" splight/adminWebsite/page.html
+# sed -i "s/:[0-9]\{8\}-[0-9]\{6\}/:$TAG/g" *prod.yml
 
 echo "--------------------------------------------"
-echo "Building eu.gcr.io/jacquev6-0001/splight-admin:$TAG"
+echo "Building jacquev6/splight:admin-$TAG"
 echo "--------------------------------------------"
 
-docker build --file splight-admin.prod.dockerfile --tag eu.gcr.io/jacquev6-0001/splight-admin:$TAG .
+docker build --file splight-admin.prod.dockerfile --tag jacquev6/splight:admin-$TAG .
 
 echo "--------------------------------------------"
-echo "Building eu.gcr.io/jacquev6-0001/splight-backup:$TAG"
+echo "Building jacquev6/splight:backup-$TAG"
 echo "--------------------------------------------"
 
-docker build --file splight-backup.prod.dockerfile --tag eu.gcr.io/jacquev6-0001/splight-backup:$TAG .
+docker build --file splight-backup.prod.dockerfile --tag jacquev6/splight:backup-$TAG .
 
 echo "--------------------------------------------"
 echo "Images built"
 echo "--------------------------------------------"
 
-docker push eu.gcr.io/jacquev6-0001/splight-admin:$TAG
-docker push eu.gcr.io/jacquev6-0001/splight-backup:$TAG
+docker push jacquev6/splight:admin-$TAG
+docker push jacquev6/splight:backup-$TAG
 
 echo "--------------------------------------------"
 echo "Images pushed"
 echo "--------------------------------------------"
 
-function k8s_config {
-  local F
-  for F in *.prod.yml
-  do
-    echo "---"
-    cat $F
-  done
-}
+# function k8s_config {
+#   local F
+#   for F in *.prod.yml
+#   do
+#     echo "---"
+#     cat $F
+#   done
+# }
 
-k8s_config | kubectl apply -f -
+# k8s_config | kubectl apply -f -
