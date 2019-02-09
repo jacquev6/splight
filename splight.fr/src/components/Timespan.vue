b6fc084645b12b173ca10f9db4d320ead32ca20b
Vincent Jacques <vincent@vincent-jacques.net>
2019-02-09 08:33:36 +0100

    Restore previous ads layout in Timespan.vue

--- splight.fr/src/components/Timespan.vue
+++ splight.fr/src/components/Timespan.vue
@@ -5,11 +5,43 @@
     </template>
 
     <template v-slot:content>
-      <p>This is a timespan starting at {{ startDate }}, with duration {{ duration }}, and with tags {{ tags }}.</p>
-      <p>{{ city }}</p>
+      <b-row class="d-lg-none">
+        <b-col>
+          <div class="sp-advertisement">
+            <p>Publicité</p>
+            <p><img class="img-fluid" alt="Publicité 468x60" src="../assets/ads/468x60.png" /></p>
+          </div>
+        </b-col>
+      </b-row>
+
+      <b-row>
+        <b-col>
+          <p>This is a timespan starting at {{ startDate }}, with duration {{ duration }}, and with tags {{ tags }}.</p>
+          <p>{{ city }}</p>
+        </b-col>
+
+        <b-col lg="3" xl="2" class="d-none d-lg-inline-block">
+          <div class="sp-advertisement">
+            <p>Publicité</p>
+            <p><img class="img-fluid" alt="Publicité 160x600" src="../assets/ads/160x600.png" /></p>
+          </div>
+        </b-col>
+      </b-row>
     </template>
   </sp-layout>
 </template>
+
+<style>
+  .sp-advertisement {
+    color: gray;
+    padding-bottom: 20px;
+  }
+
+  .sp-advertisement p {
+    margin: 0;
+    font-size: .8em;
+  }
+</style>
 
 <script>
 import gql from 'graphql-tag'
