2346bc25eb822a2e381cb49217b93f20160afb4e
Vincent Jacques <vincent@vincent-jacques.net>
2019-02-09 08:33:08 +0100

    Move stuff from App to Layout

--- splight.fr/src/components/Layout.vue
+++ splight.fr/src/components/Layout.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <b-container>
     <vue-headful :title="title"></vue-headful>
     <b-jumbotron :lead="lead" header-level="4">
       <template slot="header">
@@ -8,9 +8,47 @@
       </template>
     </b-jumbotron>
 
-    <slot name="content"></slot>
-  </div>
+    <div>
+      <slot name="content"></slot>
+    </div>
+
+    <p>Copyright <a href="mailto:vincent@vincent-jacques.net">Vincent Jacques</a> 2018-2019.</p>
+  </b-container>
 </template>
+
+<style lang="scss">
+$sp-main-color-very-light: #9AB2E8;
+// $sp-main-color-light: #5E81D2;
+$sp-main-color: #3660C1;
+// $sp-main-color-dark: #103FAC;
+$sp-main-color-very-dark: #0A2B77;
+$sp-complement-color-very-light: #FFDF9F;
+// $sp-complement-color-light: #FFCB62;
+$sp-complement-color: #FFBA31;
+// $sp-complement-color-dark: #FFAA00;
+$sp-complement-color-very-dark: #B17600;
+
+body {
+  background-color: $sp-main-color-very-light;
+}
+
+.container {
+  background-color: #fff;
+  border: solid 3px $sp-complement-color;
+}
+
+.jumbotron {
+  background-color: $sp-complement-color-very-light;
+}
+
+h1 {
+  color: $sp-main-color-very-dark;
+}
+
+a {
+  color: $sp-main-color;
+}
+</style>
 
 <script>
 export default {
